974b18c386bba67e2016b0cfe9d48d80ef8f90c6
Mishal Rai <minkumarrai@gmail.com>
2020-06-19 09:56:44 +0545

    complete 04 lecture

--- src/index.js
+++ src/index.js
@@ -5,44 +5,23 @@ window.onload = function(){
     var context = canvas.getContext("2d");
 
     /* 
-        Line Caps -> context.lineCap = "";
-        - butt -> default
+        Line Caps -> context.lineJoin = "miter| bevel | round";
+        - miter -> default
+        - bevel
         - round
-        - square
     */
 
     /* First line with line cap butt(default)*/
     context.beginPath();
-    context.lineCap = "butt";
     context.strokeStyle = "red";
+    // context.lineJoin = "bevel";
+    context.lineJoin = "round";
     context.lineWidth = 20;
-    context.moveTo(100, 30);
-    context.lineTo(100, 300);
+    context.moveTo(50, 50);
+    context.lineTo(300, 50);
+    context.lineTo(300, 300);
+    context.lineTo(50, 300);
+    context.lineTo(50, 450);
     context.stroke();
-
-
-    /* Second line with line cap square*/
-    context.beginPath();
-    context.lineCap = "square";
-    context.lineWidth = 20;
-    context.moveTo(250, 30);
-    context.lineTo(250, 300);
-    context.strokeStyle = "blue";
-    context.stroke();
-    
-    
-    /* Third line with line cap round*/
-    context.beginPath();
-    context.lineCap = "round";
-    context.lineWidth = 20;
-    context.strokeStyle = "green";
-    context.moveTo(400, 30);
-    context.lineTo(400, 300);
-    context.stroke();
-
 }
 
-
-/*
- *  Note => square and round line cap increase the size of the line 
- */
