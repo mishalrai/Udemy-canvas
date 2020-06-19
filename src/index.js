54e6ba2c185588bbbdc575aea20cd41db9dd0a9a
Mishal Rai <minkumarrai@gmail.com>
2020-06-19 08:56:51 +0545

    complete lecture 02 drawing complec line

--- src/index.js
+++ src/index.js
@@ -6,10 +6,30 @@ window.onload = function(){
 
     //Draw line steps
     context.beginPath(); // reset the context state
-    context.strokeStyle = "red"; //color of the line
-    context.lineWidth = 5; //thickness of the line
-    context.moveTo(50, 50); // move(x, y) -> starting point of the line
-    context.lineTo(300, 50); //line(x, y) -> end point of the line
+    context.moveTo(30, 30); // move(x, y) -> starting point of the line
+    context.lineTo(80, 80); //line(x, y) -> end point of the line
+    context.lineTo(130, 30); //line(x, y) -> end point of the line
+    context.lineTo(180, 80); //line(x, y) -> end point of the line
+    context.lineTo(230, 30); //line(x, y) -> end point of the line
     context.stroke();  // draw the line
+
+   /*  //Draw line steps
+    context.beginPath(); // reset the context state
+    context.moveTo(80, 80); // move(x, y) -> starting point of the line
+    context.lineTo(130, 30); //line(x, y) -> end point of the line
+    context.stroke();  // draw the line
+
+
+    //Draw line steps
+    context.beginPath(); // reset the context state
+    context.moveTo(130, 30); // move(x, y) -> starting point of the line
+    context.lineTo(180, 80); //line(x, y) -> end point of the line
+    context.stroke();  // draw the line
+
+    //Draw line steps
+    context.beginPath(); // reset the context state
+    context.moveTo(180, 80); // move(x, y) -> starting point of the line
+    context.lineTo(230, 30); //line(x, y) -> end point of the line
+    context.stroke();  // draw the line */
 
 }
