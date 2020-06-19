6aae5d25810de373945787b25a2fea542a36ac4f
Mishal Rai <minkumarrai@gmail.com>
2020-06-19 08:44:14 +0545

    complete leacture 1

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
-var canvas = document.getElementById("canvas");
-var context = canvas.getContext("2d");
+window.onload = function(){
 
-context.fillStyle = "red";
-context.fillRect(0, 0, 200, 200);
+    //Definations
+    var canvas = document.getElementById("canvas");
+    var context = canvas.getContext("2d");
+
+    //Draw line steps
+    context.beginPath(); // reset the context state
+    context.strokeStyle = "red"; //color of the line
+    context.lineWidth = 5; //thickness of the line
+    context.moveTo(50, 50); // move(x, y) -> starting point of the line
+    context.lineTo(300, 50); //line(x, y) -> end point of the line
+    context.stroke();  // draw the line
+
+}
