8e65c3881f99956e7ca400ee2c94839703baeb76
Mishal Rai <minkumarrai@gmail.com>
2020-06-23 08:43:58 +0545

    l08-lecture complete

--- src/index.js
+++ src/index.js
@@ -5,23 +5,32 @@ window.onload = function(){
     var context = canvas.getContext("2d");
 
     /* 
-       arc
-       arc(x,y,r,sa,ea, counterClockeWise)
-
-       - x => x co-ordinate
-       - y => y co-ordinate
-       - radius => circle radius
-       - sa => start angle
-       - ea => end angle
-       - counterClockwise
+        Quadratic Curves 
+        quadraticCurveTo( controlX, controlY, endX, endY)
+        - controlX => 
     */
 
-    var radian = Math.PI / 180;
+    context.beginPath();
+    context.strokeStyle = "red";
+    context.lineWidth = 5;
+    context.moveTo(50, 200);
+    context.quadraticCurveTo( 150, 50, 250, 200)
+    context.stroke();
+
 
     context.beginPath();
-    context.lineWidth = 30;
-    context.strokeStyle = "red";
-    context.arc(200, 200, 50, 0*radian, 180*radian, true);
+    context.strokeStyle = "blue";
+    context.lineWidth = 1;
+    context.moveTo(50, 200);
+    context.lineTo( 150, 50);
+    context.lineTo(250, 200);
+    context.stroke();
+
+    context.beginPath();
+    context.strokeStyle = "black";
+    context.lineWidth = 1;
+    context.moveTo(50, 125);
+    context.lineTo(250, 125);
     context.stroke();
 }
 
