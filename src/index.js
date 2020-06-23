2e74adf0e1fe61496bac0b9ff5ba695d456c690e
Mishal Rai <minkumarrai@gmail.com>
2020-06-23 09:06:41 +0545

    l09 lecture complete

--- src/index.js
+++ src/index.js
@@ -5,32 +5,24 @@ window.onload = function(){
     var context = canvas.getContext("2d");
 
     /* 
-        Quadratic Curves 
-        quadraticCurveTo( controlX, controlY, endX, endY)
-        - controlX => 
+        Bezier Curve
+        bezierCurveTo( controlX1, controlY1, controlX2, controlY2, endX, endY)
     */
 
     context.beginPath();
     context.strokeStyle = "red";
     context.lineWidth = 5;
     context.moveTo(50, 200);
-    context.quadraticCurveTo( 150, 50, 250, 200)
+    context.bezierCurveTo( 100, 50, 200, 50, 250, 200)
     context.stroke();
-
 
     context.beginPath();
     context.strokeStyle = "blue";
     context.lineWidth = 1;
     context.moveTo(50, 200);
-    context.lineTo( 150, 50);
+    context.lineTo( 100, 50);
+    context.lineTo(200, 50);
     context.lineTo(250, 200);
-    context.stroke();
-
-    context.beginPath();
-    context.strokeStyle = "black";
-    context.lineWidth = 1;
-    context.moveTo(50, 125);
-    context.lineTo(250, 125);
     context.stroke();
 }
 
