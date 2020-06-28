e54fae712f30b6ec9ea8f53e5ecba2e5d4c61d8a
Mishal Rai <minkumarrai@gmail.com>
2020-06-28 11:03:00 +0545

    l13 lecture complate

--- src/index.js
+++ src/index.js
@@ -5,24 +5,13 @@ window.onload = function(){
     var ctx = canvas.getContext("2d");
 
     /* COLORS */
-    var lightCellColor = "#ddb180";
-    var darkCellColor = "#7c330c";
-
-    ctx.fillStyle = darkCellColor;
-    ctx.fillRect( 30, 30, 440, 440);
-
+    var radiant = Math.PI/180;
+    
     ctx.beginPath();
-    ctx.lineWidth = 5;
-    ctx.strokeRect( 47.5, 47.5, 405, 405);
-
-
-    for( y=0; y <8; y++){
-        for( x=0; x<8; x++){
-            var fillColor = (x+y)%2 == 0 ? lightCellColor : darkCellColor;
-            ctx.fillStyle = fillColor;
-            ctx.fillRect(50 + x*50, 50 + y*50, 50, 50);
-         }
-    }
+    ctx.fillStyle = "black";
+    ctx.arc( 200, 200, 150, 0*radiant, 180*radiant, true);
+    ctx.stroke();
+    ctx.fill();
     
 }
 
