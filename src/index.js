aa039cc98fab010045516e5d5e39e7f83f32d20e
Mishal Rai <minkumarrai@gmail.com>
2020-07-01 09:03:06 +0545

    l14 lecture complete

--- src/index.js
+++ src/index.js
@@ -8,10 +8,23 @@ window.onload = function(){
     var radiant = Math.PI/180;
     
     ctx.beginPath();
-    ctx.fillStyle = "black";
-    ctx.arc( 200, 200, 150, 0*radiant, 180*radiant, true);
+    ctx.moveTo(200, 200);
+    ctx.lineTo(272, 286);
+    ctx.arc( 200, 200, 112, 50*radiant, 300*radiant, false);
+    ctx.lineTo(200, 200);
+    ctx.fillStyle = "orange";
+    ctx.strokeStyle = "orange";
     ctx.stroke();
     ctx.fill();
+
+    ctx.beginPath();
+    ctx.arc(180, 144, 10, 0*radiant, 360*radiant, false);
+    ctx.fillStyle = "white";
+    ctx.strokeStyle = "white";
+    ctx.fill();
+    ctx.strokeStyle();
+
+    ctx.stroke();
     
 }
 
