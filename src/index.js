f2ecc98712e618c00792a9f4a7375b17e96fd683
Mishal Rai <minkumarrai@gmail.com>
2020-07-13 10:10:54 +0545

    lecture 15 complete

--- src/index.js
+++ src/index.js
@@ -4,46 +4,51 @@ window.onload = function(){
     var canvas = document.getElementById("canvas");
     var ctx = canvas.getContext("2d");
 
-    /* COLORS */
+    // Polygon variables
+    var sides = 7;
+    var radius = 100;
+    var centerX = 250;
+    var centerY = 250;
+    var startAngle = 20;
+
+    var angle = (2*Math.PI) / sides;
+
+    // Start drawing
+    ctx.beginPath();
+    ctx.strokeStyle = "red";
+    ctx.lineWidth = 5;
+    ctx.lineJoin = "round";
+
+    //Begining point coordinates
+    var beginX = centerX + radius * Math.cos(startAngle);
+    var beginY = centerY - radius * Math.sin(startAngle);
+
+    ctx.moveTo(beginX, beginY);
+
+    
+
+    //Drawing lines
+    for( var i =1; i<=sides; i++){
+        //Current point's coordinates
+        var currentAngle = startAngle + i * angle;
+        var currentPointX = centerX + radius * Math.cos(currentAngle);
+        var currentPointY = centerY - radius * Math.sin(currentAngle);
+
+        //Draw the line
+        ctx.lineTo( currentPointX, currentPointY);
+    }
+    ctx.closePath();
+    ctx.stroke();
+
     var radiant = Math.PI/180;
-    
-    ctx.beginPath();
-    ctx.moveTo(200, 200);
-    ctx.lineTo(272, 286);
-    ctx.arc( 200, 200, 112, 50*radiant, 300*radiant, false);
-    ctx.lineTo(200, 200);
-    ctx.fillStyle = "orange";
-    ctx.strokeStyle = "orange";
-    ctx.stroke();
-    ctx.fill();
 
     ctx.beginPath();
-    ctx.arc(180, 144, 10, 0*radiant, 360*radiant, false);
-    ctx.fillStyle = "white";
-    ctx.strokeStyle = "white";
-    ctx.fill();
+    ctx.lineWidth = 2;
+    ctx.strokeStyle = "#000";
+    ctx.arc(centerX, centerY, radius, 0, 360*radius, false);
+    ctx.arc(centerX, centerY, 1, 0, 360*radius, false);
     ctx.stroke();
-    
 
-    ctx.beginPath();
-    ctx.strokeStyle = "red";
-    ctx.fillStyle = "red";
-    ctx.arc(400, 100, 80, 120*radiant, 300*radiant, false);
-    ctx.stroke();
-    ctx.fill(); 
-    
-    ctx.beginPath();
-    ctx.strokeStyle = "red";
-    ctx.fillStyle = "red";
-    ctx.arc(400, 100, 80, 60*radiant, 240*radiant, false);
-    ctx.stroke();
-    ctx.fill();
 
-    ctx.beginPath();
-    ctx.strokeStyle = "white";
-    ctx.fillStyle = "white";
-    ctx.arc(390, 60, 7, 0, 360*radiant, false);
-    ctx.fill();
-    ctx.stroke();
 }
 
