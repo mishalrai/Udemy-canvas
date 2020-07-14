581cb1b43f04b3fd407629d9c516f2d024bc24d4
Mishal Rai <minkumarrai@gmail.com>
2020-07-14 09:11:22 +0545

    l18 lecture complete

--- src/index.js
+++ src/index.js
@@ -4,18 +4,37 @@ window.onload = function(){
     var canvas = document.getElementById("canvas");
     var ctx = canvas.getContext("2d");
 
-    // must pass font size with font family
-    ctx.font= "40px Verdana";
+    // ctx.font = "font-style font-weight font-size font-family";
 
-    // Context.fillText( text, xCoordinate, yCoordinate, maxWidth);
-    var fText = "Canvas text";
-    ctx.fillText( fText, 100, 100,);
+    //font-style
+        // -normal (the default)
+        // -italic
+        // - oblique
+        // - inherit (style comes from the parent element)
 
-    ctx.fillText( "Testing width parameter", 100, 300, 100);
+    
+    // font-weight
+        // - normal (the default)
+        // - bold
+        // - bolder
+        // - lighter
+        // - 100 | 200 | 300 | 400 | 500 | 600 | 700 | 800 | 900
+        // - inherit (style comes from the parent element)
 
-    // Context.strokeText(text, xCoordinate, yCoordinate, maxWidth);
-    var sText = "Stroke Text";
-    ctx.strokeText( sText, 100, 200);
+    // font-size
+        // - xx-small | x-small | small | smaller | medium | large | x-large | xx-large
+        // - exact number in px (pixels), pt (points) or em (ems)
+        // - inherit;
+
+    // font-family
+        // - fomily-name (linke "times", "courier", "arial"...)
+        // - generic-family (like "serif", "sans-serif", "cursive")
+        // - inherit (style comes from the parent element)
+
+    var text = "I am testing text styling";
+    ctx.font = "normal bolder xx-large courier";
+    ctx.fillText(text, 10, 100);
+
 
 }
 
