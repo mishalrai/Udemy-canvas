138dd4c2f5b5604c5e52fa0a02212b0a47735e0d
Mishal Rai <minkumarrai@gmail.com>
2020-07-14 08:38:13 +0545

    l16 lecture complete

--- src/index.js
+++ src/index.js
@@ -4,51 +4,26 @@ window.onload = function(){
     var canvas = document.getElementById("canvas");
     var ctx = canvas.getContext("2d");
 
-    // Polygon variables
-    var sides = 7;
-    var radius = 100;
-    var centerX = 250;
-    var centerY = 250;
-    var startAngle = 20;
+    /*
+     Shadow attributes on canvas
+     -shadowColor
+     -shadowOffsetX
+     -shadowOffsetY
+     -shadowBlur
+     */
 
-    var angle = (2*Math.PI) / sides;
-
-    // Start drawing
     ctx.beginPath();
-    ctx.strokeStyle = "red";
-    ctx.lineWidth = 5;
-    ctx.lineJoin = "round";
-
-    //Begining point coordinates
-    var beginX = centerX + radius * Math.cos(startAngle);
-    var beginY = centerY - radius * Math.sin(startAngle);
-
-    ctx.moveTo(beginX, beginY);
-
+    ctx.fillStyle = "green";
     
-
-    //Drawing lines
-    for( var i =1; i<=sides; i++){
-        //Current point's coordinates
-        var currentAngle = startAngle + i * angle;
-        var currentPointX = centerX + radius * Math.cos(currentAngle);
-        var currentPointY = centerY - radius * Math.sin(currentAngle);
-
-        //Draw the line
-        ctx.lineTo( currentPointX, currentPointY);
-    }
-    ctx.closePath();
+    
+    // Shadow
+    ctx.shadowColor = "#000";
+    ctx.shadowOffsetX = 5;
+    ctx.shadowOffsetY = 5;
+    ctx.shadowBlur = 10;
+    ctx.rect(100, 100, 200, 200);
+    
     ctx.stroke();
-
-    var radiant = Math.PI/180;
-
-    ctx.beginPath();
-    ctx.lineWidth = 2;
-    ctx.strokeStyle = "#000";
-    ctx.arc(centerX, centerY, radius, 0, 360*radius, false);
-    ctx.arc(centerX, centerY, 1, 0, 360*radius, false);
-    ctx.stroke();
-
-
+    ctx.fill();
 }
 
