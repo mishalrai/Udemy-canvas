2fa8afe90524e858e55810680c0c8363b6a94506
Mishal Rai <minkumarrai@gmail.com>
2020-07-16 07:55:40 +0545

    l21 lecture complete

--- src/index.js
+++ src/index.js
@@ -4,58 +4,29 @@ window.onload = function(){
     var canvas = document.getElementById("canvas");
     var ctx = canvas.getContext("2d");
     
-    // context.textAlign = "center | end | left | right | start";
-    // Specifics how text is aligned horizontally
-    // the default value is "start"
+     /* 
+      Shadow attributes on canvas
+      -shadowColor
+      -shadowOffsetX
+      -shadowOffsetY
+      -shadowBlur
+     */
 
-    // context.textBaseline = "alphabetic | top | hanging | middle | bottom";
-    // Specifies how text is aligned vertically
-    // The default value is "alphbetic"
-
-
-    // Vertical Reference line
-    ctx.beginPath();
-    ctx.moveTo( 250, 0);
-    ctx.lineTo(250, 500);
-    ctx.stroke();
-
-    ctx.font = "normal 400 18px monospace";
-    
-    ctx.textAlign = "start";
-    ctx.fillText("start", 250, 35);
-
-    ctx.textAlign = "center";
-    ctx.fillText( "center", 250, 50);
-
-    ctx.textAlign = "end";
-    ctx.fillText("end", 250, 65);
-
-    ctx.textAlign = "left";
-    ctx.fillText( "left", 250, 80);
-
-    ctx.textAlign = "right";
-    ctx.fillText("right", 250, 95);
+    ctx.font = "normal 400 30px times";
+    ctx.shadowColor = "red";
+    ctx.shadowOffsetX = 10;
+    ctx.shadowOffsetY = 10;
+    ctx.shadowBlur = 10;
+    ctx.fillText("Testing text shadow", 20, 40);
 
 
-    ctx.beginPath();
-    ctx.moveTo(0, 250);
-    ctx.lineTo(500, 250);
-    ctx.stroke();
+    ctx.font = "normal 400 45px times";
+    ctx.shadowColor = "black";
+    ctx.shadowOffsetX = 10;
+    ctx.shadowOffsetY = 10;
+    ctx.shadowBlur = 10;
+    ctx.strokeText("Testing text shadow", 20, 200);
 
-    ctx.textBaseline = "alphabetic";
-    ctx.fillText("Alphabtic", 100, 250);
-
-    ctx.textBaseline = "top";
-    ctx.fillText("top", 150, 250);
-    
-    ctx.textBaseline = "hanging";
-    ctx.fillText("hanging",250, 250);
-
-    ctx.textBaseline = "middle";
-    ctx.fillText("middle", 350, 250);
-
-    ctx.textBaseline = "bottom";
-    ctx.fillText("bottom", 450, 250);
 
 }
 
