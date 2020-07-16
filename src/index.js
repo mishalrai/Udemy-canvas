291b2a530026e19744d0e9dafe34f579bc327904
Mishal Rai <minkumarrai@gmail.com>
2020-07-16 07:49:03 +0545

    l20 lecture complete

--- src/index.js
+++ src/index.js
@@ -4,21 +4,58 @@ window.onload = function(){
     var canvas = document.getElementById("canvas");
     var ctx = canvas.getContext("2d");
     
+    // context.textAlign = "center | end | left | right | start";
+    // Specifics how text is aligned horizontally
+    // the default value is "start"
 
-    draw3DText("Hello Mishal Rai", 20, 100, "normal bolder 45px courier", "red", 15);
+    // context.textBaseline = "alphabetic | top | hanging | middle | bottom";
+    // Specifies how text is aligned vertically
+    // The default value is "alphbetic"
 
-    function draw3DText( text, x, y, style, color, size){
-        var i = 0;
-        ctx.font = style;
-        ctx.fillStyle = "black";
-        for( i; i< size; i++){
-            ctx.fillText(text, x+i, y+i);
-        }
 
-        ctx.fillStyle = color;
-        ctx.fillText( text, x, y);
-    }   
+    // Vertical Reference line
+    ctx.beginPath();
+    ctx.moveTo( 250, 0);
+    ctx.lineTo(250, 500);
+    ctx.stroke();
 
+    ctx.font = "normal 400 18px monospace";
+    
+    ctx.textAlign = "start";
+    ctx.fillText("start", 250, 35);
+
+    ctx.textAlign = "center";
+    ctx.fillText( "center", 250, 50);
+
+    ctx.textAlign = "end";
+    ctx.fillText("end", 250, 65);
+
+    ctx.textAlign = "left";
+    ctx.fillText( "left", 250, 80);
+
+    ctx.textAlign = "right";
+    ctx.fillText("right", 250, 95);
+
+
+    ctx.beginPath();
+    ctx.moveTo(0, 250);
+    ctx.lineTo(500, 250);
+    ctx.stroke();
+
+    ctx.textBaseline = "alphabetic";
+    ctx.fillText("Alphabtic", 100, 250);
+
+    ctx.textBaseline = "top";
+    ctx.fillText("top", 150, 250);
+    
+    ctx.textBaseline = "hanging";
+    ctx.fillText("hanging",250, 250);
+
+    ctx.textBaseline = "middle";
+    ctx.fillText("middle", 350, 250);
+
+    ctx.textBaseline = "bottom";
+    ctx.fillText("bottom", 450, 250);
 
 }
 
