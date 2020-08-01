f6aa11febd70fb1dd3164c348069a4b07e531ff4
Mishal Rai <minkumarrai@gmail.com>
2020-08-01 22:08:33 +0545

    l23 lecture complete

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
-var canvas = document.getElementById("canvas");
-var context = canvas.getContext("2d");
 
-context.fillStyle = "red";
-context.fillRect(0, 0, 200, 200);
+
+window.onload = function(){
+
+    var canvas = document.getElementById("canvas");
+    var context = canvas.getContext("2d");
+
+    var img = new Image();
+    img.src = 'src/test.png';
+    
+    // drawImage( img, dx, dy )
+    // drawImage( img, dx, dy, dw, dy )
+    // drawImage( img, sx, sy, sw, sy, dx, dy, dw, dy, )
+
+    img.onload = function(){
+        
+        // context.drawImage( img, 50, 50, 1000, 1000);
+
+        context.drawImage( img, 304, 30, 150, 322, 50, 50, 150, 322);
+
+    }
+    
+}
