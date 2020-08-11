df76acd5930e370246c345671feb67334eb7b2c4
Mishal Rai <minkumarrai@gmail.com>
2020-08-11 08:00:10 +0545

    l26 lecture complete

--- src/index.js
+++ src/index.js
@@ -5,62 +5,30 @@ window.onload = function(){
     var canvas = document.getElementById("canvas");
     var context = canvas.getContext("2d");
 
-    var img = new Image();
-    img.src = 'src/test.png';
+    // context.save()     -> Saving the context state-canvas
+    // context.restore()  -> restoring the state from the state stack
+
     
-    // drawImage( img, dx, dy )
-    // drawImage( img, dx, dy, dw, dy )
-    // drawImage( img, sx, sy, sw, sy, dx, dy, dw, dy, )
+    context.fillStyle = "red";
+    context.fillRect( 240, 100, 120, 120);
+    context.save();
 
 
-  
-    img.onload = function(){
-        
-        var imgWidth = img.width;
-        var imgHeight = img.height;
-        context.drawImage( img, 50, 50);
+    context.fillStyle = "green";
+    context.fillRect( 390, 100, 120, 120);
+    context.save();
 
-        // Image Data functions
-        // - createImageData() Creates a new, blank Image Object
-        // - getImageData() Returns and ImageData object that copies the pixel data for the specified reactangle on 
-        // - putImageData() puts the image data (from a specified ImageData object) back onto the canvas
+    context.fillStyle = "blue";
+    context.fillRect( 540, 100, 120, 120);
+    context.save();
 
-        var imageData = context.getImageData( 50, 50, imgWidth, imgHeight);  //(x, y, imgWidth, imgHeight) => x and are starting point of  image width and height
-    
-        console.log( imageData, 'Image Data');
+    context.restore()
+    context.fillRect( 240, 300, 120, 120);
 
-        // R - The color red (from 0-255);
-        // G - The color green (from 0-255);
-        // R - The color blue (from 0-255);
-        // R - The alpa channel (from 0-255, o is transparent and 255 is fully visible);
+    context.restore()
+    context.fillRect( 390, 300, 120, 120);
 
-        for( var i=0; i< imageData.data.length; i+=4){
-            // imageData.data[i] = 0;      // Red
-            // imageData.data[i+1] = 0;    // Geen
-            // imageData.data[i+2] = 0;    // Blue
-            // imageData.data[i+3] = 255;  // Alpa
-           
-            // Decrease opacity of image
-            // imageData.data[i+3] = 50;  // Alpa
+    context.restore()
+    context.fillRect( 540, 300, 120, 120);
 
-            //Inverse Filter
-            //  imageData.data[i] = 255 - imageData.data[i] ;      // Red
-            // imageData.data[i+1] = 255 - imageData.data[i+1];    // Geen
-            // imageData.data[i+2] = 255 - imageData.data[i+2];    // Blue
-
-
-            //Black and white filter
-            
-            var avarage = (imageData.data[i] + imageData.data[i + 1] + imageData.data[i + 2]) / 3;
-
-            imageData.data[i] = avarage;      // Red
-            imageData.data[i+1] = avarage;    // Geen
-            imageData.data[i+2] = avarage;    // Blue 
-        }
-
-        context.putImageData(imageData, 50, 50);
-
-
-    }
-    
 }
