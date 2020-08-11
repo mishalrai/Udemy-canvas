7d0fc25b64ffd82e233ea824e450a846526f3bff
Mishal Rai <minkumarrai@gmail.com>
2020-08-11 08:39:55 +0545

    l28 lecture complete

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
-var canvas = document.getElementById("canvas");
-var context = canvas.getContext("2d");
 
-context.fillStyle = "red";
-context.fillRect(0, 0, 200, 200);
+window.onload = function(){
+    var canvas = document.getElementById("canvas");
+    var context = canvas.getContext("2d");
+
+    // context.cratePattern(image, repetition);
+    // repetition:
+    // -repeat: both directions
+    // -repeat-x: horizontal only
+    // -repeat-y: vertical only
+    // -no-repeat: neither
+
+    var patternImage = new Image();
+    patternImage.src = 'src/pattern.png';
+
+    patternImage.onload = function(){
+        // var pattern = context.createPattern( patternImage, 'repeat');
+        var pattern = context.createPattern( patternImage, 'repeat-x');
+        context.fillStyle = pattern;
+        context.fillRect( 0, 0, canvas.width, canvas.height);
+    };
+
+}
