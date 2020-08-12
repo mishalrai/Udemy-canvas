9410952a0f8ffc6c59809d0238d80649ec6175c2
Mishal Rai <minkumarrai@gmail.com>
2020-08-12 08:02:17 +0545

    l29 lecture complete

--- src/index.js
+++ src/index.js
@@ -2,22 +2,35 @@
 window.onload = function(){
     var canvas = document.getElementById("canvas");
     var context = canvas.getContext("2d");
+    
+    var gap = 80;
 
-    // context.cratePattern(image, repetition);
-    // repetition:
-    // -repeat: both directions
-    // -repeat-x: horizontal only
-    // -repeat-y: vertical only
-    // -no-repeat: neither
+    // context.createLinearGradient(x1, y1, x2, y2);
+    // context.crateRadialGradiant( x1, y1, r1, x2, y2, r2);
 
-    var patternImage = new Image();
-    patternImage.src = 'src/pattern.png';
 
-    patternImage.onload = function(){
-        // var pattern = context.createPattern( patternImage, 'repeat');
-        var pattern = context.createPattern( patternImage, 'repeat-x');
-        context.fillStyle = pattern;
-        context.fillRect( 0, 0, canvas.width, canvas.height);
-    };
+
+
+    var radialGradient = context.createRadialGradient(200, 200, 400, canvas.width -gap, canvas.height -gap, 400);
+    radialGradient.addColorStop(0, 'red');
+    radialGradient.addColorStop(0.5, 'black');
+    radialGradient.addColorStop(1, 'blue');
+
+
+    context.fillStyle = radialGradient;
+    context.rect( gap/2, gap/2, (canvas.width-gap) / 2, canvas.height-gap );
+    context.stroke();
+    context.fill();
+
+    var linearGradient = context.createLinearGradient( gap/2, gap/2, canvas.width -gap, canvas.height -gap);
+    linearGradient.addColorStop(0, 'red');
+    linearGradient.addColorStop(0.5, 'yellow');
+    linearGradient.addColorStop(1, 'blue');
+
+    context.fillStyle = linearGradient;
+    context.fillRect( gap/2 + (canvas.width-gap) / 2, gap/2, (canvas.width-gap) / 2, canvas.height-gap );
+   
+
+
 
 }
