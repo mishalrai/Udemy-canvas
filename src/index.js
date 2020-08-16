78ec6e8ef81d8e7b7b3e3f65878637e739422adb
Mishal Rai <minkumarrai@gmail.com>
2020-08-16 19:58:59 +0545

    l33 lecture complete

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
-var canvas = document.getElementById("canvas");
-var context = canvas.getContext("2d");
 
-context.fillStyle = "red";
-context.fillRect(0, 0, 200, 200);
+
+window.onload = function () {
+
+    var canvas = document.getElementById("canvas");
+    var context = canvas.getContext("2d");
+
+
+    // context.scalte( horzontalScaleFactor, verticalScaleFoctor)  -> scales the content 
+
+
+    context.fillRect( 0, 0, 100, 100);
+
+    context.scale( 2, 2);
+    context.fillRect( 65, 65, 100, 100);
+
+}
