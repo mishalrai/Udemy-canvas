ef910ada8fb8cd184faa23382ebce68788ad0560
Mishal Rai <minkumarrai@gmail.com>
2020-08-17 08:27:44 +0545

    l138 lecture complete

--- src/index.js
+++ src/index.js
@@ -5,19 +5,33 @@ window.onload = function () {
     var canvas = document.getElementById("canvas");
     var context = canvas.getContext("2d");
 
+    // What is velocity?
+    // Velocity is the rate of change in an object's position
+    // Velocity has a magnitude (speed) and a direction
+    // Velocity is a vector quantity.
+    // Velocity is represented by the formula.
+    // velocity = Δx/Δt
+
     var ball = new Ball(20, 'green');
     ball.x = canvas.width/2;
     ball.y = canvas.height/2;
     ball.context = context;
     ball.draw();
 
+    //Velocity
+    ball.vx = 2;
+    ball.vy = 2;
     
-    // requestAnimationFrame(animationLoop);
+    requestAnimationFrame(animationLoop);
 
     function animationLoop() {
         
         // Clear Canvas
         context.clearRect( 0, 0, canvas.width, canvas.height );
+
+        ball.x = ball.x + ball.vx;
+        ball.y = ball.y + ball.vy;
+        ball.draw();
         
         requestAnimationFrame(animationLoop);
     };
