77544da1c3f712da2df4c402192131f28641d4b0
Mishal Rai <minkumarrai@gmail.com>
2020-08-18 07:57:34 +0545

    l42 lecture complete

--- src/index.js
+++ src/index.js
@@ -13,11 +13,21 @@ window.onload = function () {
         ball.y = 200;
         ball.context = context;
         ball.draw();
+
+    //Ball 2
+    var ball2 = new Ball(20, 'red');
+        ball2.x = 30;
+        ball2.y = 480;
+        ball2.context = context;
+        ball2.draw();
     
 
     //Velocity
     ball.vx = 5;
     ball.vy = 0;
+   
+    ball2.vx = 5;
+    ball2.vy = -7;
 
     requestAnimationFrame(animationLoop);
 
@@ -28,12 +38,19 @@ window.onload = function () {
 
         
         // Update
-        ball.vy = ball.vy + g;
 
-        ball.x = ball.x + ball.vx;
-        ball.y = ball.y + ball.vy;
+            //ball
+            ball.vy = ball.vy + g;
+            ball.x = ball.x + ball.vx;
+            ball.y = ball.y + ball.vy;
+
+            //ball2
+            ball2.vy = ball2.vy + g;
+            ball2.x = ball2.x + ball2.vx;
+            ball2.y = ball2.y + ball2.vy;
 
         ball.draw();
+        ball2.draw();
     
         
         requestAnimationFrame(animationLoop);
